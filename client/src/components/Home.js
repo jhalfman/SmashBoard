668cc2803997969f25b3337852bee0eabe3da8c3
Jordan Halfman <jordanhalfman@gmail.com>
2023-01-19 13:01:28 -0700

    testing deployment with git pushing

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = ({currentUser}) => {
   return (
     <div>
-      {currentUser ? `Welcome ${currentUser.username}` : "Home"}
+      {currentUser ? `Welcome ${currentUser.username}!!` : "Home"}
     </div>
   )
 }
