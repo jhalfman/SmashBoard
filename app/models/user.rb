8bb96b677c9ea0e707a311c45512f8ba9c1a9dba
Jordan Halfman <jordanhalfman@gmail.com>
2023-01-19 18:43:59 -0700

    add allow blank: true to password validation so usernames can be updated without password entered

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     has_many :nights
     validates :username, uniqueness: {case_sensitive: false}
-    validates :password, length: {in: 7..15}
+    validates :password, length: {in: 7..15}, allow_blank: true
 
     has_secure_password
 end
